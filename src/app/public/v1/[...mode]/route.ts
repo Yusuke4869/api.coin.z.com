d06330115b9c776cb9ac1a46aad3c4a79c332cc2
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-07-02 16:06:47 +0900

    fix

--- src/app/public/v1/[...mode]/route.ts
+++ src/app/public/v1/[...mode]/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 
-export const cors = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET",
-  "Access-Control-Allow-Headers": "Content-Type",
-};
-
 export const GET = async (request: Request, { params }: { params: { mode: string } }) => {
   const { searchParams } = new URL(request.url);
+  const cors = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+
   const res = await fetch(`https://forex-api.coin.z.com/public/v1/${params.mode}?${searchParams.toString()}`);
   return NextResponse.json(await res.json(), { status: res.status, headers: cors });
 };
